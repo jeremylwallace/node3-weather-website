222490a8d544a3297b115c536459a47298be4426
Jeremy Wallace <Jeremy@wallace-mbp15.lan>
2019-10-13 23:27:32 -0500

    Added highs and lows to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (location, cb) => {
     } else if (response.body.error) {
         cb('Unable to find location', undefined)
     } else {
-        cb(undefined, 'Weather for ' + location.location + ': ' + response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+        cb(undefined, 'Weather for ' + location.location + ': ' + response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. The high today is expected to be ' + response.body.daily.data[0].temperatureHigh + ' degrees. The low is expected to be ' + response.body.daily.data[0].temperatureLow + ' degrees. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
     }
 })
 }
